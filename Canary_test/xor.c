e231a503b5d26fbf2c1bb32baa1d9d3b227b477b
maanitmehra <maanitm@gmail.com>
2016-03-05 06:07:56 -0500

    Upload cleaned code. Training Added.

--- Canary_test/xor.c
+++ Canary_test/xor.c
@@ -8,6 +8,12 @@
 // MACRO Definitions
 #define NUM_DIG 16
 #define THRESH 0.5
+#define TRAIN_ITER 100
+#define ENABLE 1
+#define DISABLE 0
+
+// Switch to allow training
+#define TRAINING_MODE DISABLE 
 // Defining Neurons 
 #define N1 2
 #define N2 2
@@ -45,6 +51,7 @@ void matMul (double*[], int*[], double*[], int, int, int);
 bool isNumber(char *);
 void transpose(int *[], int* [], int, int);
 void sigArr(double* [], int* [], int,int);
+void training(double* [], double* []);
 
 void initLayers(int * a[], int num_neurons)
 {
@@ -79,7 +86,7 @@ void initWts (double* theta[], int rows, int cols)
 	{
 		for (j=0; j<cols; j++)
 		{
-			theta[i][j]=rand();
+			theta[i][j]=(rand()%50)-25;
 		}
 	}
 }
@@ -211,6 +218,104 @@ void sigArr(double* in[], int* out[], int row,int col)
 	}
 }
 
+// Training code based on 
+// Andrew NG's Machine Learning Course
+// & http://mnemstudio.org/neural-networks-backpropagation-xor.htm
+
+void training(double* theta1[], double* theta2[])
+{
+	int x_train[4][3];
+	double y_train[4];
+
+	x_train[0][0]   =  1;
+    	x_train[0][1]   =  0;
+    	x_train[0][2]   =  1; 
+    	y_train[0]      =  1;
+
+    	x_train[1][0]   =  0;
+    	x_train[1][1]   =  1;
+    	x_train[1][2]   =  1; 
+    	y_train[1]      =  1;
+
+    	x_train[2][0]   =  1;
+    	x_train[2][1]   =  1;
+    	x_train[2][2]   =  1; 
+    	y_train[2]      =  0;
+
+    	x_train[3][0]   =  0;
+    	x_train[3][1]   =  0;
+    	x_train[3][2]   =  1; 
+    	y_train[3]      =  0;
+
+        // initializing the weights for each layer with random numbers
+        initWts(theta1, N1+1, N2);
+        initWts(theta2, N2+1, N3);
+
+	// training input picked at random
+	// Training Algorithm executed TRAIN_ITER times.
+	int iter;
+	int i;
+	int m;
+	int j;
+	int k;
+	double des_op;
+	int test[3];
+	double hid_lay[3][1];
+	hid_lay[0][0]=1;
+	double err=0;
+	double err_sum=0.0;
+	double calc_op;
+	for (iter=0 ; iter < TRAIN_ITER; iter++)
+	{
+		err_sum=0.0;
+		// We find that there are 4 training inputs in this case.
+		for (i=0; i<4; i++)
+		{
+			des_op = y_train[i];
+			for(m=0;m<3;m++) 
+			test[m] = x_train[i][m];
+		
+			for (m=1; m<3;m++)
+			{
+				int sum=0;
+				for (j=0; j<3; j++)
+				{
+				   sum += theta1[m-1][j]*test[j];
+				} 
+				hid_lay[m][0] = sig (sum);
+			} 
+			double sum2=0.0;
+			for (m=0; m<3;m++)
+			{
+				sum2 += theta2[0][m]*hid_lay[m][0];
+			}
+			calc_op = sig(sum2);
+			
+			err = calc_op - des_op;
+			//printf("%f" ,err);
+			err_sum +=(err*err);
+			double delta;
+
+			// begin by Calculating delta from Output to Hidden Layer
+		    	for(m = 0; m < N2; m++)
+        		{
+				delta =  err * hid_lay[m][0];
+        			theta2[0][k] = theta2[0][k] + delta;
+			}
+			// delta from hidden to first layer
+			for(i = 0; i < N2; i++)
+			{
+				delta =0;
+        			for(k = 0; k < N1; k++)
+				{
+            				delta = test[k]*err*sig(hid_lay[i][0])*(1-sig(hid_lay[i][0]));
+            				theta1[k][i] = theta1[k][i] + delta;
+        			}
+    			}
+		}// end sweep through different train_inputs
+		err_sum = err_sum/4;
+	}// end sweep through iterations
+}// end training function
 
 int main(int argc, char *argv[])
 {
@@ -232,6 +337,8 @@ int main(int argc, char *argv[])
 	initWts(theta1, N1+1, N2);
 	initWts(theta2, N2+1, N3);
 
+	if (!TRAINING_MODE)
+	{
 	theta1[0][0]=-1;
         theta1[0][1]= 2;
         theta1[0][2]= 2;
@@ -242,6 +349,33 @@ int main(int argc, char *argv[])
         theta2[0][0]=-3;
         theta2[0][1]= 2;
         theta2[0][2]= 2;
+	}
+	else
+	{
+	training(theta1, theta2);
+
+        printf("theta1:\n%f\t",theta1[0][0]);//=-1;
+        printf("%f\t",theta1[0][1]);
+        printf("%f\n",theta1[0][2]);
+        printf("%f\t",theta1[1][0]);
+        printf("%f\t",theta1[1][1]);
+        printf("%f\n",theta1[1][2]);
+	
+        printf("theta2:\n%f\t",theta2[0][0]);
+        printf("%f\t",theta2[0][1]);
+        printf("%f\n",theta2[0][2]);
+	}
+        theta1[0][1]= 2;
+        theta1[0][2]= 2;
+        theta1[1][0]= 3;
+        theta1[1][1]=-2;
+        theta1[1][2]=-2;
+
+        theta2[0][0]=-3;
+        theta2[0][1]= 2;
+        theta2[0][2]= 2;
+
+
 
 	int a;
 	int b;
@@ -276,21 +410,7 @@ int main(int argc, char *argv[])
 		a1[2][idx]=arr_b[idx];
 	}
 
-
-
 	// Forward Propogation
-	int ro;
-	int co;
-//	printf("\nPrinting a1:\n");
-        for (ro=0; ro<3;ro++)
-        {
-//                printf("%dth row:\t", (ro+1));
-                for (co=0; co<16; co++)
-                {
-//                        printf("%d  ",a1[ro][co]);
-                }
-//                printf("\n");
-        }
 
 	double *out1[N2];
 	double *out2[N3];
@@ -314,16 +434,6 @@ int main(int argc, char *argv[])
 
 	matMul(theta2, a2, out2, N3, N2+1, NUM_DIG);
 	sigArr(out2, h2, N3, NUM_DIG);
-
-//	printf("Printing output...\n");
-	for (ro = 0; ro<1; ro++)
-	{
-		for (co=0;co <NUM_DIG; co++)
-		{
-//			printf("%d",h2[ro][co]);
-		}
-//		printf("\n");
-	}
 
 	printf("%d XOR %d = %d\n",a,b ,bin2dec(h2[0]));
 
